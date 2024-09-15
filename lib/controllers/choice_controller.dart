ad43ac7970d66a82dc427c80d1b7518fcd4f3ba0
Vg34100 <pablor.341x@gmail.com>
2024-09-14 22:02:33 -0700

    choice model setup

---
+++ lib/controllers/choice_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:indecision_machine/models/choice_model.dart';
+
+class ChoiceController extends ChangeNotifier {
+  List<Choice> choices = [];
+}
---
+++ lib/models/choice_model.dart
@@ -0,0 +1,11 @@
+class Choice {
+  String id; // Identifier
+  String name;
+  int weight;
+
+  Choice({
+    required this.id,
+    required this.name,
+    required this.weight,
+  });
+}
