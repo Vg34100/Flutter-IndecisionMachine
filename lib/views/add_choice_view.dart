cdbe3073de4a17c5cc6035894d8522d6e5cffbdf
Vg34100 <pablor.341x@gmail.com>
2024-09-15 22:27:49 -0700

    stayle change

--- lib/views/add_choice_view.dart
+++ lib/views/add_choice_view.dart
@@ -19,92 +19,94 @@ class _AddChoiceViewState extends State<AddChoiceView> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Material(
-        color: Colors.transparent,
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Text(
-                      'Add New Choice',
-                      style: Theme.of(context).textTheme.headlineMedium,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Material(
+          color: Colors.transparent,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        'Add New Choice',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  children: [
-                    // Icon 
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
-                      child: IconButton(
-                        icon: const Icon(Icons.check_box),
-                        onPressed: () {},
+                  Row(
+                    children: [
+                      // Icon 
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+                        child: IconButton(
+                          icon: const Icon(Icons.check_box),
+                          onPressed: () {},
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 4),
-                    // Choice Name
-                    Expanded(
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Choice Name'),
-                        validator: (value) => value!.isEmpty ? 'Enter a choice name' : null,
-                        onSaved: (value) => name = value!,
+                      const SizedBox(width: 4),
+                      // Choice Name
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(labelText: 'Choice Name'),
+                          validator: (value) => value!.isEmpty ? 'Enter a choice name' : null,
+                          onSaved: (value) => name = value!,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                const Divider(),
-                // Weight Name
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Weight Name'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) => value!.isEmpty ? 'Enter weight name' : null,
-                  onSaved: (value) => weightName = value!,
-                ),
-                const SizedBox(height: 10),
-                // Weight Amount
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Weight Amount'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) => value!.isEmpty ? 'Enter weight amount' : null,
-                  onSaved: (value) => amount = int.parse(value!),
-                ),
-                const Divider(),
-                const SizedBox(height: 30),
-                                // Action Buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      child: const Text('Cancel'),
-                      onPressed: () {
-                        Navigator.of(context).pop(); // Close the dialog
-                      },
-                    ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      child: const Text('Add'),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-                          Choice newChoice = Choice(
-                            id: const Uuid().v4(),
-                            name: name,
-                            weight: Weight(name: weightName, amount: amount,),
-                          );
-                          Navigator.of(context).pop(newChoice); // Return the new choice
-                        }
-                      },
-                    ),
-                  ],
-                ),
-            ],
-          )
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  const Divider(),
+                  // Weight Name
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Weight Name'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) => value!.isEmpty ? 'Enter weight name' : null,
+                    onSaved: (value) => weightName = value!,
+                  ),
+                  const SizedBox(height: 10),
+                  // Weight Amount
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Weight Amount'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) => value!.isEmpty ? 'Enter weight amount' : null,
+                    onSaved: (value) => amount = int.parse(value!),
+                  ),
+                  const SizedBox(height: 30),
+                  // Action Buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        child: const Text('Cancel'),
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Close the dialog
+                        },
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        child: const Text('Add'),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            Choice newChoice = Choice(
+                              id: const Uuid().v4(),
+                              name: name,
+                              weight: Weight(name: weightName, amount: amount,),
+                            );
+                            Navigator.of(context).pop(newChoice); // Return the new choice
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+              ],
+            )
+          ),
         ),
       )
     );
